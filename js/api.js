1a0a94d5858f8d9514fe869953b671b0013370a4
huboliang <huboliang_dsjt@shdsjt.cn>
2017-09-25 14:16:41 +0800

    fx

---
+++ js/api.js
@@ -0,0 +1,5 @@
+var api = 'http://192.168.20.55:8082/v1/';
+//活动详情接口
+var GetActivityDetail = 'TMoment/GetActivityDetail';
+// 获取活动报名信息-报名成员列表接口
+var GetActivityApplyList = 'TMoment/GetActivityApplyList';
