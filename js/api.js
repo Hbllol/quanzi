2971aa4117bfea3064f55445def2eb61fcd10130
huboliang <huboliang_dsjt@shdsjt.cn>
2017-09-26 10:58:30 +0800

    two

--- js/api.js
+++ js/api.js
@@ -1,5 +1,12 @@
-var api = 'http://192.168.20.55:8082/v1/';
+// var api = 'http://192.168.20.55:8082/v1/';
+var api = 'http://192.168.20.45:1685/v1/'
 //活动详情接口
 var GetActivityDetail = 'TMoment/GetActivityDetail';
 // 获取活动报名信息-报名成员列表接口
 var GetActivityApplyList = 'TMoment/GetActivityApplyList';
+// 获取圈子动态详细信息
+var GetDynamicStateDetail = 'TMoment/GetDynamicStateDetail';
+// 获取圈子动态信息-列表
+var GetDynamicStateList  ='TMoment/GetDynamicStateList'
+// 获取圈子、banner、活动的动态
+var GetDynamicState  = 'TMoment/GetDynamicState'
