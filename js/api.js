b95d369811d3a8e28b6389118555118ac3e14e1f
huboliang <huboliang_dsjt@shdsjt.cn>
2017-09-27 14:07:42 +0800

    three

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
-// var api = 'http://192.168.20.55:8082/v1/';
-var api = 'http://192.168.20.45:1685/v1/'
+var api = 'http://192.168.20.55:8082/v1/';
+// var api = 'http://192.168.20.45:1685/v1/'
 //活动详情接口
 var GetActivityDetail = 'TMoment/GetActivityDetail';
 // 获取活动报名信息-报名成员列表接口
